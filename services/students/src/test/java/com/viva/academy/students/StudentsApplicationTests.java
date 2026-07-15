17401fe8574212adf8ece0bb9c1a09627f1d0305
vetriselvan-pv <learnerkid11@gmail.com>
2026-07-15 09:50:43 +0530

    init repo

---
+++ services/students/src/test/java/com/viva/academy/students/StudentsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.viva.academy.students;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
